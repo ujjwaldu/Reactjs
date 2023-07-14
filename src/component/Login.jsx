c7c9aae1a71b1bc1e7dadfe0591730df6257e45d
UJJWAL DUBEY <127877364+ujjwaldu@users.noreply.github.com>
2023-07-15 02:08:44 +0530

    login data sent to api

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-
+import { useState } from 'react'  //12th vdo of rajiv sir and to replace api given by mishra mama
+import axios from 'axios'
 export default function Login() {
+    const [state, setstate] = useState({
+        name:'',
+        username:'',
+        email:'',
+        password:''
+    })
+    const [msg,setmsg]=useState('')
+    const hanldler=(event)=>{
+        const {name, value}=event.target
+        setstate({...state,[name]:value})
+    }
+
+    const savedata=(event)=>{
+        event.preventDefault();
+        axios.post('https://www.haveinlist.com/api/register.php',state) 
+        .then((res)=>{
+            console.log(res);
+            setmsg('data sent')
+        })       
+    }
+
   return (
         <>
             <div className="container">
-            <form className='outbox' align='center'>
+            <form className='outbox' align='center' onSubmit={savedata}>
             <div className="row">
                 <div className="col-md-12">
                     login
@@ -13,31 +34,32 @@ export default function Login() {
                 <div className="row">
                     <div className="col-md-12">
                         <span>Name</span>
-                        <div><input type="text" placeholder='Write your name' /></div>
+                        <div><input onChange={hanldler} type="text" placeholder='Write your name' name='name' /></div>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-12">
                         <span>username</span>
-                        <div><input type="text" placeholder='Write a username' /></div>
+                        <div><input onChange={hanldler} type="text" placeholder='Write a username' name='username' /></div>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-12">
                         <span>Email</span>
-                        <div><input type="email" placeholder='Write email' /></div>
+                        <div><input onChange={hanldler} type="email" placeholder='Write email' name='email'/></div>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-12">
                         <span>password</span>
-                        <div><input type="password" placeholder='write password here' /></div>
+                        <div><input onChange={hanldler} type="password" placeholder='write password here' name='password'/></div>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-12">
                        <div><input type="submit" value="login" /></div>
                     </div>
+                    {msg? <div className='alert alert-success'>{msg}</div>:''}
                 </div>
                 </form>
             </div>
